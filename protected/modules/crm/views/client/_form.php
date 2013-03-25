e0c523397e05520a56ef495e17f546219ae99924
Ruslan Fadeev <fadeevr@gmail.com>
2013-03-25 16:58:24 +0400

    Update form

--- protected/modules/crm/views/client/_form.php
+++ protected/modules/crm/views/client/_form.php
@@ -13,6 +13,7 @@ $form = $this->beginWidget(
         'htmlOptions' => array('class' => 'well')
     )
 );
+Yii::app()->clientScript->registerCss('input', 'input,select,textarea{width: 100%}');
 Yii::app()->clientScript->registerScript('scroll', '$("html, body").animate({scrollTop: $("#client-form").position().top-60}, "slow");', CClientScript::POS_READY);
 echo $form->errorSummary($model);
 if ($model->isNewRecord && !isset($_GET['id']) || !$_GET['id']) {
@@ -22,12 +23,12 @@ if ($model->isNewRecord && !isset($_GET['id']) || !$_GET['id']) {
 }
 ?>
 <div class="row-fluid">
-    <div class="span2"><?php echo $form->textAreaRow($model, 'name_company', array('style' => 'width: 100%')); ?></div>
-    <div class="span3"><?php echo $form->textAreaRow($model, 'name_contact', array('style' => 'width: 100%')); ?></div>
-    <div class="span2"><?php echo $form->textAreaRow($model, 'phone', array('style' => 'width: 100%')); ?></div>
-    <div class="span2"><?php echo $form->textAreaRow($model, 'email', array('style' => 'width: 100%')); ?></div>
-    <div class="span1"><?php echo $form->textFieldRow($model, 'time_zone', array('style' => 'width: 90%')); ?></div>
-    <div class="span1"><?php echo $form->textFieldRow($model, 'site', array('style' => 'width: 100%')); ?></div>
+    <div class="span2"><?php echo $form->textAreaRow($model, 'name_company'); ?></div>
+    <div class="span3"><?php echo $form->textAreaRow($model, 'name_contact'); ?></div>
+    <div class="span2"><?php echo $form->textAreaRow($model, 'phone'); ?></div>
+    <div class="span2"><?php echo $form->textAreaRow($model, 'email'); ?></div>
+    <div class="span1"><?php echo $form->textFieldRow($model, 'time_zone'); ?></div>
+    <div class="span1"><?php echo $form->textFieldRow($model, 'site'); ?></div>
     <div class="span1"><?php echo $form->dropDownListRow(
             $model,
             'cp',
@@ -36,32 +37,31 @@ if ($model->isNewRecord && !isset($_GET['id']) || !$_GET['id']) {
         ); ?></div>
 </div>
 <div class="row-fluid">
-    <div class="span2"><?php echo $form->typeAheadRow($model, 'city', array('source' => array_values($model->getList('city'))), array('class' => 'span12')); ?></div>
-    <div class="span3"><?php echo $form->textFieldRow($model, 'address', array('style' => 'width: 100%')); ?></div>
-    <div class="span1"><?php echo $form->typeAheadRow($model, 'product', array('source' => array_values($model->getList('product'))), array('style' => 'width: 100%')); ?></div>
-    <div class="span1"><?php echo $form->typeAheadRow($model, 'sponsor', array('source' => array_values($model->getList('sponsor'))), array('style' => 'width: 100%')); ?></div>
+    <div class="span2"><?php echo $form->typeAheadRow($model, 'city', array('source' => array_values($model->getList('city')))); ?></div>
+    <div class="span3"><?php echo $form->textFieldRow($model, 'address'); ?></div>
+    <div class="span1"><?php echo $form->typeAheadRow($model, 'product', array('source' => array_values($model->getList('product')))); ?></div>
+    <div class="span1"><?php echo $form->typeAheadRow($model, 'sponsor', array('source' => array_values($model->getList('sponsor')))); ?></div>
     <div class="span2"><?php echo $form->datepickerRow(
             $model,
             'next_time',
             array(
-                'style'   => 'width: 100%',
                 'options' => array('format' => 'yyyy-mm-dd'),
                 'events' => array('hide' => 'js:function(e){var c=$("#Client_next_time");if(c.val().length)c.val(c.val() + " ' . date('H:i:00') . '")}')
             )
         ); ?></div>
-    <div class="span1"><?php echo $form->dropDownListRow($model, 'create_user_id', CHtml::listData(User::model()->cache(3600)->findAll(), 'id', 'username'), array('style' => 'width: 98%')); ?></div>
-    <div class="span1"><?php echo $form->dropDownListRow($model, 'status', $model->statusMain->getList(), array('style' => 'width: 100%')); ?></div>
+    <div class="span1"><?php echo $form->dropDownListRow($model, 'create_user_id', CHtml::listData(User::model()->cache(3600)->findAll(), 'id', 'username')); ?></div>
+    <div class="span1"><?php echo $form->dropDownListRow($model, 'status', $model->statusMain->getList()); ?></div>
 </div>
 <div class="row-fluid">
-    <div class="span4"><?php echo $form->textAreaRow($model, 'client_request', array('rows' => 6, 'cols' => 50, 'style' => 'width: 100%')); ?></div>
-    <div class="span4"><?php echo $form->textAreaRow($model, 'comment_history', array('rows' => 6, 'cols' => 50, 'style' => 'width: 100%')); ?></div>
-    <div class="span4"><?php echo $form->textAreaRow($model, 'comment_fail', array('rows' => 6, 'cols' => 50, 'style' => 'width: 100%')); ?></div>
+    <div class="span4"><?php echo $form->textAreaRow($model, 'client_request', array('rows' => 6, 'cols' => 50)); ?></div>
+    <div class="span4"><?php echo $form->textAreaRow($model, 'comment_history', array('rows' => 6, 'cols' => 50)); ?></div>
+    <div class="span4"><?php echo $form->textAreaRow($model, 'comment_fail', array('rows' => 6, 'cols' => 50)); ?></div>
 </div>
 <div class="row-fluid">
-    <div class="span4"><?php echo $form->textAreaRow($model, 'comment_review', array('rows' => 6, 'style' => 'width: 100%')); ?></div>
-    <div class="span4"><?php echo $form->textAreaRow($model, 'description_production', array('rows' => 6, 'style' => 'width: 100%')); ?></div>
-    <div class="span4"><?php echo $form->textAreaRow($model, 'photo', array('rows' => 2, 'style' => 'width: 100%'));
-        echo $form->textFieldRow($model, 'contract_copy', array('style' => 'width: 100%')); ?>
+    <div class="span4"><?php echo $form->textAreaRow($model, 'comment_review', array('rows' => 6)); ?></div>
+    <div class="span4"><?php echo $form->textAreaRow($model, 'description_production', array('rows' => 6)); ?></div>
+    <div class="span4"><?php echo $form->textAreaRow($model, 'photo', array('rows' => 2));
+        echo $form->textFieldRow($model, 'contract_copy'); ?>
     </div>
 </div>
 <div class="form-actions">

7c15ce6b88a88d0589b794c48c6d42a38010a550
Ruslan Fadeev <fadeevr@gmail.com>
2013-03-29 18:20:05 +0400

    Fix

--- protected/modules/crm/views/client/_formOrder.php
+++ protected/modules/crm/views/client/_formOrder.php
@@ -25,10 +25,10 @@ if ($order->isNewRecord) {
     <div class="span1"><?php echo $form->dropDownListRow($order, "[$id]photo", array(Yii::t('CrmModule.client', 'Нет'), Yii::t('CrmModule.client', 'Есть')), array('empty' => Yii::t('zii', 'Not set'))); ?></div>
     <div class="span2"><?php echo $form->dropDownListRow($order, "[$id]contract_copy", array('' => Yii::t('zii', 'Not set'), 1 => Yii::t('CrmModule.client', 'Есть'), 0 => Yii::t('CrmModule.client', 'Нет'))); ?></div>
     <div class="span2"><?php echo $form->typeAheadRow($order, "[$id]product", array('source' => array_values($order->getList('product')))); ?></div>
-    <div class="span2"><?php echo $form->typeAheadRow($order, "[$id]sponsor", array('source' => array_values($order->getList('sponsor'))), array('style' => 'width: 93%')); ?></div>
+    <div class="span2"><?php echo $form->typeAheadRow($order, "[$id]sponsor", array('source' => array_values($order->getList('sponsor'))), array('style' => 'width: 89%')); ?></div>
 </div>
 <div class="row-fluid">
     <div class="span4"><?php echo $form->textAreaRow($order, "[$id]comment_review", array('rows' => 6)); ?></div>
     <div class="span4"><?php echo $form->textAreaRow($order, "[$id]comment_fail", array('rows' => 6)); ?></div>
-    <div class="span4"><?php echo $form->textAreaRow($order, "[$id]description_production", array('rows' => 6, 'style' => 'width: 96%')); ?></div>
+    <div class="span4"><?php echo $form->textAreaRow($order, "[$id]description_production", array('rows' => 6, 'style' => 'width: 94%')); ?></div>
 </div>

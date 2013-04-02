a8823fc2e3cb0d66509341ce7f2c4f98ed776273
Ruslan Fadeev <fadeevr@gmail.com>
2013-04-02 17:33:38 +0400

    Template update

--- protected/modules/crm/views/client/update.php
+++ protected/modules/crm/views/client/update.php
@@ -8,8 +8,7 @@
 $this->breadcrumbs = array(
 	Yii::t('CrmModule.client', 'Clients') => array('admin'),
     $client->cache(3600)->project->name => array('admin', 'id' => $client->project_id),
-	$client->client_id => array('view', 'id' => $client->client_id),
-	Yii::t('zii', 'Update'),
+    $client->client_id . ' ' . Yii::t('zii', 'Update'),
 );
 
 echo $this->renderPartial('_form', array('client' => $client, 'orders' => $orders));

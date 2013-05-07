89c3a201c722c3beb5df0b66893da1bb781748dc
Ruslan Fadeev <fadeevr@gmail.com>
2013-05-07 16:11:23 +0400

    Added check if guest

--- protected/modules/crm/controllers/ClientOrderController.php
+++ protected/modules/crm/controllers/ClientOrderController.php
@@ -2,10 +2,26 @@
 
 class ClientOrderController extends Controller
 {
+    /**
+     * @return array a list of filter configurations.
+     */
+    public function filters()
+    {
+        return array(
+            'ajaxOnly + updateEditable',/** @see CController::filterAjaxOnly */
+            array('auth.filters.AuthFilter - updateEditable')/** @see AuthFilter */
+        );
+    }
+
     public function actionUpdateEditable()
     {
         Yii::import('bootstrap.widgets.TbEditableSaver');
         $es = new TbEditableSaver('ClientOrder');
+        $es->onBeforeUpdate = function($event) {
+            if (Yii::app()->user->getIsGuest()) {
+                $event->sender->error(Yii::t('yii', 'Login Required'));
+            }
+        };
         $es->update();
     }
 }

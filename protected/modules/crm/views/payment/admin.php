e14747fe347952d2c0cc18cb3d8dfa799f99870b
Ruslan Fadeev <fadeevr@gmail.com>
2013-05-08 11:43:29 +0400

    Clean code

--- protected/modules/crm/views/payment/admin.php
+++ protected/modules/crm/views/payment/admin.php
@@ -25,56 +25,56 @@ $this->widget(
         'ajaxUrl'      => $this->createUrl('payment/admin', array('id' => $model->project_id)),
         'columns'      => array(
             array(
-                'name' => 'client_id',
-                'value' => 'CHtml::link($data->client_id, array("client/update", "id" => $data->client_id), array("target" => "_blank"))',
-                'type' => 'raw',
+                'name'        => 'client_id',
+                'value'       => 'CHtml::link($data->client_id, array("client/update", "id" => $data->client_id), array("target" => "_blank"))',
+                'type'        => 'raw',
                 'htmlOptions' => array('style' => 'width: 25px'),
             ),
             array(
-                'name' => 'partner.project.name',
-                'header' => Yii::t('CrmModule.payment', 'Project'),
+                'name'    => 'partner.project.name',
+                'header'  => Yii::t('CrmModule.payment', 'Project'),
                 'visible' => $model->project_id == null,
             ),
             array(
-                'name' => 'partner.name',
+                'name'   => 'partner.name',
                 'header' => Yii::t('CrmModule.payment', 'Partner'),
                 'filter' => CHtml::activeDropDownList($model, 'partner_id', CHtml::listData(CrmHelper::partners($model->project_id), 'id', 'name'), array('empty' => '')),
             ),
             array(
-                'name' => 'name_company',
+                'name'  => 'name_company',
                 'class' => 'bootstrap.widgets.TbEditableColumn',
             ),
             array(
-                'name' => 'name_contact',
+                'name'  => 'name_contact',
                 'class' => 'TbEditableColumn',
             ),
             array(
-                'name' => 'client.city',
-                'class' => 'TbEditableColumn',
-                'filter' => CHtml::activeTextField($model, 'city'),
+                'name'     => 'client.city',
+                'class'    => 'TbEditableColumn',
+                'filter'   => CHtml::activeTextField($model, 'city'),
                 'editable' => array('url' => $this->createUrl('client/updateEditable')),
             ),
             array(
-                'name' => 'comments',
-                'class' => 'PopoverColumn',
+                'name'     => 'comments',
+                'class'    => 'PopoverColumn',
                 'editable' => array(
-                    'type' => 'textarea',
+                    'type'      => 'textarea',
                     'placement' => 'left',
                     'options'   => array('showbuttons' => true)
                 ),
             ),
             array(
-                'name' => 'payment_amount',
-                'class' => 'TbEditableColumn',
+                'name'     => 'payment_amount',
+                'class'    => 'TbEditableColumn',
                 'editable' => array('options' => array('inputclass' => 'input-small'))
             ),
             'payment',
-            'payment_remain',
+            //'payment_remain',
             //'calculation_percent'
             'agent_comission_percent',
             array(
-                'name' => 'agent_comission_amount',
-                'class' => 'TbEditableColumn',
+                'name'     => 'agent_comission_amount',
+                'class'    => 'TbEditableColumn',
                 'editable' => array('options' => array('inputclass' => 'input-small'))
             ),
             'agent_comission_received',
